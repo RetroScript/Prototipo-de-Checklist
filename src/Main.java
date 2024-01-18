49723c5bf1aa523855d998f7ccfd7b0ca91f152f
RetroScript <andrefrancelino06@gmail.com>
2024-01-18 20:44:01 -0300

    Classe main Alterada, novos recusos implementados
    Foi adicionado diferentes menus através do uso de um CardLayout e um JPanel

--- src/Main.java
+++ src/Main.java
@@ -1,146 +1,86 @@
-import java.awt.Canvas;
+
+import java.awt.BorderLayout;
+import java.awt.CardLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 
-import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class Main implements ActionListener{
+public class Main extends JPanel implements ActionListener, ItemListener{
 	
-	public static JPanel panel;
+	public JPanel cardsP;
+	public final int WIDTH = 500;
+	public final int HEIGHT = 350;
 	
-//	public static JButton buttonS;
+	public Main() {
+		super(new BorderLayout());
+		
+		JPanel cbPanel = new JPanel();
+		cbPanel.setPreferredSize(new Dimension(0,150));
+		cbPanel.setBackground(Color.lightGray);
+		
+		String cbOptions[] = {"CARD1", "CARD2"};
+		JComboBox cb = new JComboBox(cbOptions);
+		cb.setPreferredSize(new Dimension(300,100));
+		cb.addItemListener(this);
+		cbPanel.add(cb);
+		
+		cardsP = new JPanel(new CardLayout());
+		cardsP.setBackground(Color.pink);
+		cardsP.setPreferredSize(new Dimension(0, 200));
+		
+		JPanel card1 = new JPanel();
+		card1.setBackground(Color.cyan);
+		
+		JPanel card2 = new JPanel();
+		card2.setBackground(Color.magenta);
+		
+		
+		cardsP.add(card1, cbOptions[0]);
+		cardsP.add(card2, cbOptions[1]);
+		
+		
+		setPreferredSize(new Dimension(WIDTH, HEIGHT));
+		add(cbPanel, BorderLayout.NORTH);
+		add(cardsP, BorderLayout.SOUTH);
+		
+		
+	}
 	
-	public static JButton createB;
-	public static JButton deleteB;
-	public static JButton editB;
-	
-	
-	//JFrame é um container
-	//JFrame is a container
-	public static JFrame frame;
+	public void initFrame() {
+		JFrame frame = new JFrame("CheckList Program!");
+		
+		JComponent customCP = new Main();
+		frame.setContentPane(customCP);
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
+		frame.setVisible(true);
+	}
 	
 	public static void main(String [] args) {
-		
 		Main m = new Main();
-		
-		frame = new JFrame("Checklist!");
-	
-//		
-//		//the Canva size, inherited from this class, is set		
-//		m.setPreferredSize(new Dimension(300,300));
-//		
-//		//this add the Canva component to the Frame
-//		frame.add(m);
-		
-		frame.setSize(new Dimension(300,300));
-		frame.setLayout(null);
-		frame.setVisible(true);
-		frame.setResizable(false);
-		frame.setLocationRelativeTo(null);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-		
-//		//Actually, even reading the docs, I still
-//		//need to know how it works
-//		frame.pack();
-		
-		
-		//TEST PANEL -> proceed to test later
-		
-//		panel = new JPanel();
-//		panel.setSize(new Dimension(300, 300));
-		
-		
-//		//TEST BUTTON
-//		buttonS = new JButton();
-//		buttonS.setVisible(true);
-//		
-//		buttonS.setBounds(100, 100, 100, 50);
-//		buttonS.setText("hehe");
-//		buttonS.setFocusable(false);
-//		buttonS.addActionListener(m);
-//		buttonS.setBackground(Color.decode("#FF0000"));
-//		frame.add(buttonS);
-
-		
-//		//somehow this didn't work, maybe I should isolate this and test more
-//		button.setLocation(0, 0);
-//		button.setPreferredSize(new Dimension(50,50));
-		
-		//FUNCTIONAL BUTTON
-		m.loadMainMenu();
-		
+		m.initFrame();
 	}
 	
-	public void loadMainMenu() {
-		
-
-		createB = new JButton();
-		createB.setVisible(true);
-		createB.setText("CREATE");
-		createB.setBounds(0, 0, 100, 50);
-		createB.addActionListener(this);
-		frame.add(createB);
-		
-		deleteB = new JButton();
-		deleteB.setVisible(true);
-		deleteB.setText("DELETE");
-		deleteB.setBounds(100, 0, 100, 50);
-		deleteB.addActionListener(this);
-		frame.add(deleteB);
-	
-		editB = new JButton();
-		editB.setVisible(true);
-		editB.setText("EDIT");
-		editB.setBounds(200, 0, 100, 50);
-		editB.addActionListener(this);
-		frame.add(editB);
-		
-	}
-	
-	public void loadCreationMenu() {
-		
-		
-		
-		frame.remove(createB);
-		frame.remove(deleteB);
-		frame.remove(editB);
-		
-		System.out.println("loading creation menu");
+	public void actionPerformed(ActionEvent e) {
 		
 	}
 
 	@Override
-	//if some action occurs this method is called
-	public void actionPerformed(ActionEvent e) {
-		
-		//if the button is pressed
+	public void itemStateChanged(ItemEvent e) {
 
-//		if(e.getSource() == buttonS)
-//		{		
-//			
-//			//do something
-//			System.out.println("testing, working");
-//			
-//			
-//			
-//		}
-		
-		if(e.getSource() == createB) {
-			
-			//start the checklist creation menu
-			
-			loadCreationMenu();
-			
-		}
+		//Porque tem que ser chamado assim?
+		CardLayout cl = (CardLayout)(cardsP.getLayout());
+		cl.show(cardsP, (String) e.getItem());
 		
 	}
-	
-	
-	
 
 }
