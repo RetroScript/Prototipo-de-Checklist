de2673af8b154e03846c14e5e088ec04453ddd63
RetroScript <andrefrancelino06@gmail.com>
2024-02-04 20:19:27 -0300

    Alterações na disposição dos componentes

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@
 import java.awt.BorderLayout;
 import java.awt.CardLayout;
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -23,7 +24,7 @@ public class Main extends JPanel implements ActionListener, ItemListener{
 	public JPanel card1;
 	public JPanel card2;
 	public final int WIDTH = 500;
-	public final int HEIGHT = 350;
+	public final int HEIGHT = 300;
 	
 	public JTextField nomeAtual;
 	public JTextField nomeAnterior;
@@ -38,15 +39,20 @@ public class Main extends JPanel implements ActionListener, ItemListener{
 	public Main() {
 		super(new BorderLayout());
 		
-		JPanel cbPanel = new JPanel();
-		cbPanel.setPreferredSize(new Dimension(0,150));
+		JPanel cbPanel = new JPanel(/*new BorderLayout()*/);
+		cbPanel.setLayout(new BoxLayout(cbPanel, BoxLayout.X_AXIS));
+		cbPanel.setPreferredSize(new Dimension(0,100));
 		cbPanel.setBackground(Color.lightGray);
 		
 		String cbOptions[] = {"CHECKLIST ATUAL", "CHECKLIST ANTERIOR"};
 		JComboBox cb = new JComboBox(cbOptions);
-		cb.setPreferredSize(new Dimension(300,100));
+		cb.setMaximumSize(new Dimension(300,50));
+		cb.setAlignmentY(Component.CENTER_ALIGNMENT);
 		cb.addItemListener(this);
+
+		cbPanel.add(Box.createHorizontalGlue());
 		cbPanel.add(cb);
+		cbPanel.add(Box.createHorizontalGlue());
 		
 		cardsP = new JPanel(new CardLayout());
 		cardsP.setBackground(Color.pink);
@@ -55,16 +61,14 @@ public class Main extends JPanel implements ActionListener, ItemListener{
 		//mostre a Checklist Atual e suas tasks
 		//Caso não haja nenhuma ainda, sugira criar um checkList
 		card1 = new JPanel();
-		card1.setLayout(new BoxLayout(card1, BoxLayout.PAGE_AXIS));
+		card1.setLayout(new BoxLayout(card1, BoxLayout.Y_AXIS));
 		card1.setBackground(Color.cyan);
 		
 		JPanel card1Buttons= new JPanel();
 		card1Buttons.setLayout(new BoxLayout(card1Buttons, BoxLayout.LINE_AXIS));
 		card1Buttons.setBackground(Color.lightGray);
 		
-		nomeAtual = new JTextField(20);
-		nomeAtual.setBackground(Color.orange);
-		nomeAtual.addActionListener(this);	
+		
 		
 		criar = new JButton("Criar Nova checkList");
 		criar.setFocusable(false);
@@ -73,11 +77,19 @@ public class Main extends JPanel implements ActionListener, ItemListener{
 		concluir.setFocusable(false);
 		concluir.addActionListener(this);
 		
+		nomeAtual = new JTextField(20);
+		nomeAtual.setMaximumSize(new Dimension(450,100));
+		nomeAtual.setBackground(Color.orange);
+		nomeAtual.addActionListener(this);	
+		
 		task1 = new JTextField(20);
+		task1.setMaximumSize(new Dimension(450,50));
 		task2 = new JTextField(20);
+		task2.setMaximumSize(new Dimension(450,50));
 		task3 = new JTextField(20);
+		task3.setMaximumSize(new Dimension(450,50));
 		
-		
+		card1.add(Box.createRigidArea(new Dimension(0,5)));
 		card1.add(nomeAtual);
 		card1.add(Box.createRigidArea(new Dimension(0,5)));
 		card1.add(task1);
@@ -85,14 +97,15 @@ public class Main extends JPanel implements ActionListener, ItemListener{
 		card1.add(task2);
 		card1.add(Box.createRigidArea(new Dimension(0,5)));
 		card1.add(task3);
+		card1.add(Box.createRigidArea(new Dimension(0,5)));
 		
-		card1Buttons.add(Box.createRigidArea(new Dimension(25,0)));
-		card1Buttons.add(criar);
 		card1Buttons.add(Box.createHorizontalGlue());
-		card1Buttons.add(concluir);
+		card1Buttons.add(criar);
 		card1Buttons.add(Box.createRigidArea(new Dimension(25,0)));
+		card1Buttons.add(concluir);
+		card1Buttons.add(Box.createHorizontalGlue());
 		
-		card1.add(Box.createRigidArea(new Dimension(0,15)));
+		
 		card1.add(card1Buttons);
 		
 		
@@ -113,11 +126,9 @@ public class Main extends JPanel implements ActionListener, ItemListener{
 		add(cbPanel, BorderLayout.NORTH);
 		add(cardsP, BorderLayout.SOUTH);
 		
-//		add(cbPanel);
-//		add(cardsP);
+
 	}
 
-	
 	
 	
 	public void initFrame() {
